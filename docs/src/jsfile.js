ac5a1bba7efdacf33e91cd784e0470aa76fa0494
Anna <annk2@pdx.edu>
2022-06-01 07:01:17 -0700

    added contact form

--- docs/src/jsfile.js
+++ docs/src/jsfile.js
@@ -1,36 +1,21 @@
 let form = document.querySelector("form");
-let username = document.querySelector("#username");
+let username = document.querySelector("#name");
 let email = document.querySelector("#email");
-let password = document.querySelector("#password");
-let yes = document.querySelector("#yes");
-let no = document.querySelector("#no");
-let date = document.querySelector("#date");
+// let password = document.querySelector("#password");
+// let date = document.querySelector("#date");
 
 form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
 	// Add your code here
-	if (!username.value || !email.value || !password.value)
-		console.warn("You must enter some data to submit this form");
-	else {
-		console.log("======== Form Submission ========");
-		console.log("   Username:", username.value);
-		console.log("   Email:", email.value);
+	// if (!username.value || !email.value)
+	// 	console.warn("You must enter some data to submit this form");
+	// else {
+	// 	console.log("======== Form Submission ========");
+	// 	console.log("   Username:", username.value);
+	// 	console.log("   Email:", email.value); }
 
-		if (yes.checked) {
-			console.log("   Newsletter: Yes");
-		} else if (no.checked) {
-			console.log("   Newsletter: No");
-		} else {
-			console.log("   Newsletter: no submission");
-		}
-
-		if (!date.value) {
-			console.log("   Date: no submission");
-		} else {
-			console.log("   Date:", date.value);
-		}
-	}
+	console.log("add repeated data values here");
 }
 
 // let userInput = document.querySelector("#userInput1");
