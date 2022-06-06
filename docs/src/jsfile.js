b196834bf86e0ebf35db576b54f642da9ae1b6a4
Anna <annk2@pdx.edu>
2022-06-06 16:14:20 -0700

    cleaned up and added more responsiveness

--- docs/src/jsfile.js
+++ docs/src/jsfile.js
@@ -1,4 +1,4 @@
-let form = document.querySelector("form"); //need?
+let form = document.querySelector("form");
 let formElem = document.querySelectorAll(".form-element");
 let fullName = document.querySelector("#name");
 let email = document.querySelector("#email");
@@ -8,30 +8,17 @@ let confirmEmail = document.querySelector("#confirm-email");
 let confirmMessage = document.querySelector("#confirm-message");
 let confirm = document.querySelector("#confirm");
 let confirmation = document.querySelector(".confirmation");
-// let submit = document.querySelector("#submit");
-// let password = document.querySelector("#password");
-// let date = document.querySelector("#date");
 
 confirm.addEventListener("click", handleClick);
-confirmation.addEventListener("click", handleSubmitClick);
+confirmation.addEventListener("click", handleSubmit);
 
 function handleClick(event) {
-	// if (!fullName.value || !email.value || !message.value) {
-	// 	const warning = document.createElement("div");
-	// 	warning.class = "alert alert-warning";
-	// 	warning.setAttribute("role", "alert");
-	// 	warning.textContent = "You must fill out all fields before submitting.";
-	// 	form.append(warning);
-	// 	return;
-	// }
-
 	formElem.forEach((element) => {
 		element.style.display = "none";
 	});
 
 	const SubmitBtn = document.createElement("INPUT");
 	SubmitBtn.className = "btn btn-primary";
-	SubmitBtn.setAttribute("type", "submit");
 	SubmitBtn.value = "Submit";
 
 	confirmName.textContent = `Name: ${fullName.value}`;
@@ -41,11 +28,8 @@ function handleClick(event) {
 	confirmation.append(SubmitBtn);
 }
 
-function handleSubmitClick(event) {
+function handleSubmit(event) {
 	confirmation.style.display = "none";
-	// confirmName.style.display = "none";
-	// confirmEmail.style.display = "none";
-	// confirmMessage.style.display = "none";
 
 	const thanks = document.createElement("div");
 	thanks.textContent =
@@ -54,15 +38,3 @@ function handleSubmitClick(event) {
 
 	form.append(thanks);
 }
-
-// let userInput = document.querySelector("#userInput1");
-// let copy = document.querySelector("#copy");
-// let output = document.querySelector("#output");
-
-// // add an event listener on the target element
-// copy.addEventListener("click", handleClick);
-
-// // callback function to handle event
-// function handleClick(event) {
-// 	output.textContent = userInput.value;
-// }
