c2d8e7e28b7492ac16d618cd92c52931ffa7c0c9
Anna <annk2@pdx.edu>
2022-06-06 12:30:40 -0700

    added confirmation functionality to contact form

--- docs/src/jsfile.js
+++ docs/src/jsfile.js
@@ -1,21 +1,58 @@
-let form = document.querySelector("form");
-let username = document.querySelector("#name");
+let form = document.querySelector("form"); //need?
+let formElem = document.querySelectorAll(".form-element");
+let fullName = document.querySelector("#name");
 let email = document.querySelector("#email");
+let message = document.querySelector("#message");
+let confirmName = document.querySelector("#confirm-name");
+let confirmEmail = document.querySelector("#confirm-email");
+let confirmMessage = document.querySelector("#confirm-message");
+let confirm = document.querySelector("#confirm");
+let confirmation = document.querySelector(".confirmation");
+// let submit = document.querySelector("#submit");
 // let password = document.querySelector("#password");
 // let date = document.querySelector("#date");
 
-form.addEventListener("submit", handleSubmit);
+confirm.addEventListener("click", handleClick);
+confirmation.addEventListener("click", handleSubmitClick);
 
-function handleSubmit(event) {
-	// Add your code here
-	// if (!username.value || !email.value)
-	// 	console.warn("You must enter some data to submit this form");
-	// else {
-	// 	console.log("======== Form Submission ========");
-	// 	console.log("   Username:", username.value);
-	// 	console.log("   Email:", email.value); }
+function handleClick(event) {
+	// if (!fullName.value || !email.value || !message.value) {
+	// 	const warning = document.createElement("div");
+	// 	warning.class = "alert alert-warning";
+	// 	warning.setAttribute("role", "alert");
+	// 	warning.textContent = "You must fill out all fields before submitting.";
+	// 	form.append(warning);
+	// 	return;
+	// }
 
-	console.log("add repeated data values here");
+	formElem.forEach((element) => {
+		element.style.display = "none";
+	});
+
+	const SubmitBtn = document.createElement("INPUT");
+	SubmitBtn.className = "btn btn-primary";
+	SubmitBtn.setAttribute("type", "submit");
+	SubmitBtn.value = "Submit";
+
+	confirmName.textContent = `Name: ${fullName.value}`;
+	confirmEmail.textContent = `Email: ${email.value}`;
+	confirmMessage.textContent = `Message: ${message.value}`;
+
+	confirmation.append(SubmitBtn);
+}
+
+function handleSubmitClick(event) {
+	confirmation.style.display = "none";
+	// confirmName.style.display = "none";
+	// confirmEmail.style.display = "none";
+	// confirmMessage.style.display = "none";
+
+	const thanks = document.createElement("div");
+	thanks.textContent =
+		"Thank you for contacting Anna. She looks forward to reading your message!";
+	thanks.className = "fs-4";
+
+	form.append(thanks);
 }
 
 // let userInput = document.querySelector("#userInput1");
